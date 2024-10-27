1bd97839b13f33a56b3c76c928d4483f1b7d4e6d
Christian Raisch <129881889+Cirrous@users.noreply.github.com>
2024-10-27 15:39:30 +0000

    Implemented password storing

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import getpass
 import hashlib
-from base64 import urlsafe_b64decode
+from base64 import urlsafe_b64encode
 import os
+from cryptography.fernet import Fernet
+import yaml
 
 class PasswordManager:
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str):
         self.file_path = file_path
         self.key = self.get_key()
+
+        if not os.path.exists(self.file_path):
+            self.initialize_file()
 
     def get_key(self) -> bytes:
 
@@ -19,6 +24,17 @@ class PasswordManager:
             key = hashlib.sha256(key.encode()).digest()
             with open(".key", "wb") as file:
                 file.write(key)
-        return urlsafe_b64decode(key)
+        return urlsafe_b64encode(key)
     
-manager = PasswordManager("file.yaml")
+    def initialize_file(self) -> None:
+        password = getpass.getpass("Erstellen Sie bitte ein Authentifizierungspasswort: ")
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        encrypted_data = Fernet(self.key).encrypt(b"{}")
+
+        with open(self.file_path, "w") as file: 
+            data = {"auth_data": hashed_password, "passwords": encrypted_data}
+            yaml.dump(data, file)
+
+    
+manager = PasswordManager("passwords.yaml")
